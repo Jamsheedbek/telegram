7d0b520a770334f32524b2d4464c2221cd345509
JamshidbekJS <jamshidbekml@gmail.com>
2022-01-28 06:30:54 +0500

    set members count

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,5 +3,5 @@ const fs = require('../lib/fsDeal');
 module.exports = (req, res) => {
     const users = JSON.parse(new fs('../models/user.json').read());
     const userInfo = req.cookies;
-    res.render('index', { user: userInfo, users: users.length });
+    res.render('index', { user: userInfo, users: users.length + 1 });
 };
